a062d411c827e2022099b8c62fcc6d14d33e9450
andyj2712 <aguilarelias536@gmail.com>
2026-01-10 01:26:41 -0600

    cnfig cors

--- config/cors.php
+++ config/cors.php
@@ -12,7 +12,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://produccion-rf-production.up.railway.app/')],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://produccion-rf-production.up.railway.app/', 'http://localhost:8080')],
 
     'allowed_origins_patterns' => [],
 
