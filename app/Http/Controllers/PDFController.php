41cc39ae9543d4dbaf2ad24eb2efc798b07e8783
Walter_RD503 <walterrd503@gmail.com>
2026-01-14 13:17:08 -0600

    Update PDFController.php

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -5,21 +5,36 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Venta; 
 use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Facades\Log; 
 
 class PDFController extends Controller
 {
     public function generarFactura($id)
     {
-        $venta = Venta::with(['detalleVentas.producto', 'empleado'])->findOrFail($id);
-        
-        $data = [
-            'venta' => $venta
-        ];
+        try {
 
-        $pdf = Pdf::loadView('pdf.factura', $data);
-        
-        $pdf->setPaper('letter', 'portrait');
+            $venta = Venta::with(['detalleVentas.producto', 'empleado'])->findOrFail($id);
+            
+            $data = [
+                'venta' => $venta
+            ];
 
-        return $pdf->stream('factura-venta-'.$venta->id_venta.'.pdf');
+            $pdf = Pdf::loadView('pdf.factura', $data);
+            
+            $pdf->setPaper('letter', 'portrait');
+
+            $pdf->render();
+
+            return $pdf->stream('factura-venta-'.$venta->id_venta.'.pdf');
+
+        } catch (\Throwable $e) {
+
+            return response()->json([
+                'STATUS' => 'ERROR FATAL AL GENERAR PDF',
+                'MENSAJE_CORTO' => $e->getMessage(),
+                'ARCHIVO_CULPABLE' => $e->getFile(),
+                'LINEA_DEL_ERROR' => $e->getLine(),
+            ], 400); 
+        }
     }
 }
