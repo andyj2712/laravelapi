7e75a3a42ca465b484ccab206e169386b097f69c
Walter_RD503 <walterrd503@gmail.com>
2026-01-13 20:39:40 -0600

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,34 +1,24 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
-    | API Cross-Origin Resource Sharing (CORS) Configuration
+    | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
     */
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'allowed_methods' => ['*'], 
 
-    'allowed_methods' => ['*'],
-
-    /*
-     * AQUÍ ESTÁ LA MAGIA:
-     * Agregamos tu localhost a la lista de permitidos explícitamente.
-     */
-    'allowed_origins' => [
-        'https://produccion-rf-production.up.railway.app',
-        'http://localhost:8080',
-    ],
+    'allowed_origins' => ['*'], 
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'], 
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
-
+    'supports_credentials' => false, 
 ];
